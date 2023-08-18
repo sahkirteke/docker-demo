a15255a4a3d6cfe10b63e02dff5d4480dc09ac82
SHK <sahhusamettin@gmail.com>
2023-08-18 22:23:50 +0300

    Dockerize Spring Boot Application

---
+++ src/test/java/com/ms/springboot/SpringbootDockerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
